45bf05e3e33099c0ee777e024bdaf6ccf397f571
Soumya Ranjan Patel <20cse347.soumyaranjanpatel@giet.edu>
2022-04-14 23:19:30 +0530

    Add files via upload

---
+++ weather/brain.js
@@ -0,0 +1,24 @@
+const inputTxt=document.querySelector('.inputTxt');
+var button=document.querySelector('.btn');
+const showData=document.querySelector('.showData');
+// const api_key="9d16de930bd84a9ada381519f20f1d45";
+const API_Key = "a0e78d3b449db7059df0a38abd3952f8";
+button.addEventListener('click',()=>{
+    const cityInput=inputTxt.value;
+    console.log(cityInput);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&APPID=${API_Key}`) 
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+        inputTxt.value="";
+        
+        showData.innerHTML=`
+                            <ul>
+                                <li class="desc">${data.weather[0].description}</li>
+                                <li class="city">${data.name}</li>
+                                <li class="temp">${data.main.temp} °C</li>
+                            </ul>
+                            `;
+    })
+
+})
